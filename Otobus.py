ff5d4812f8e7d0d53ecb2f46718cb420a1bf330e
Koray Kılıç <110056705+korayyklc@users.noreply.github.com>
2022-10-06 14:31:14 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -12,7 +12,7 @@ class Otobus:
         self.dolu_koltuk = 0
         self.bos_koltuk = kapasite
 
-    def bilet_sat(self):
+    def bilet_al(self):
         self.dolu_koltuk += 1
         self.bos_koltuk -= 1
 
